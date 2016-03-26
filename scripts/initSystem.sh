05f324ed353a62d0f2c864668d517163a9d23516
yzbx <youdaoyzbx@163.com>
2016-03-25 19:20:03 -0700

    add pkg-config and install bgs

--- scripts/initSystem.sh
+++ scripts/initSystem.sh
@@ -1,20 +1,23 @@
-#!bin/bash
+######!bin/bash
 
 #add opencv-nonfree model
 sudo add-apt-repository --yes ppa:xqms/opencv-nonfree
 sudo apt-get update
-sudo apt-get install build-essential 
-sudo apt-get install libboost-dev
-sudo apt-get install make cmake
-sudo apt-get install pkg-config
-sudo apt-get install git
-sudo apt-get install qt5-default qtcreator qt5-doc
-#sudo apt-get install libcv2.4 
-sudo apt-get install libopencv-dev
-sudo apt-get install libopencv-nonfree-dev
+sudo apt-get install build-essential -y
+sudo apt-get install libboost-dev -y
+sudo apt-get install make cmake -y
+sudo apt-get install pkg-config -y
+sudo apt-get install git -y
+sudo apt-get install qt5-default qtcreator qt5-doc -y
+#sudo apt-get install libcv2.4  -y
+sudo apt-get install libopencv-dev -y
+sudo apt-get install libopencv-nonfree-dev -y
 #sudo apt-get install motion
-sudo apt-get install nautilus-open-terminal
-sudo apt-get install cmake-gui
-sudo apt-get install tree
-sudo apt-get install libfftw3-dev
+sudo apt-get install nautilus-open-terminal -y
+sudo apt-get install cmake-gui -y
+sudo apt-get install tree -y
+sudo apt-get install libfftw3-dev -y
 #sudo apt-get install sabam
+
+mkdir ~/git
+cd ~/git & git clone https://github.com/andrewssobral/bgslibrary.git
