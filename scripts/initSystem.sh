57f612f8f93e4a5268401104bddf254eac163e9f
yzbx <youdaoyzbx@163.com>
2016-03-26 08:59:23 +0800

    initSystem.sh

---
+++ scripts/initSystem.sh
@@ -0,0 +1,20 @@
+#!bin/bash
+
+#add opencv-nonfree model
+sudo add-apt-repository --yes ppa:xqms/opencv-nonfree
+sudo apt-get update
+sudo apt-get install build-essential 
+sudo apt-get install libboost-dev
+sudo apt-get install make cmake
+sudo apt-get install pkg-config
+sudo apt-get install git
+sudo apt-get install qt5-default qtcreator qt5-doc
+#sudo apt-get install libcv2.4 
+sudo apt-get install libopencv-dev
+sudo apt-get install libopencv-nonfree-dev
+#sudo apt-get install motion
+sudo apt-get install nautilus-open-terminal
+sudo apt-get install cmake-gui
+sudo apt-get install tree
+sudo apt-get install libfftw3-dev
+#sudo apt-get install sabam
