254db99892cee73064654b32de3947fce7edfd05
yzbx <youdaoyzbx@163.com>
2016-03-31 17:40:27 +0800

    Update initSystem.sh

--- scripts/initSystem.sh
+++ scripts/initSystem.sh
@@ -2,14 +2,15 @@
 
 #add opencv-nonfree model
 sudo add-apt-repository --yes ppa:xqms/opencv-nonfree
+sudo add-apt-repository ppa:webupd8team/atom  
 sudo apt-get update
+#sudo apt-get install atom 
 sudo apt-get install build-essential -y
 sudo apt-get install libboost-dev -y
 sudo apt-get install make cmake -y
 sudo apt-get install pkg-config -y
 sudo apt-get install git -y
 sudo apt-get install qt5-default qtcreator qt5-doc -y
-#sudo apt-get install libcv2.4  -y
 sudo apt-get install libopencv-dev -y
 sudo apt-get install libopencv-nonfree-dev -y
 #sudo apt-get install motion
@@ -17,7 +18,6 @@ sudo apt-get install nautilus-open-terminal -y
 sudo apt-get install cmake-gui -y
 sudo apt-get install tree -y
 sudo apt-get install libfftw3-dev -y
-#sudo apt-get install sabam
 
 mkdir ~/git
 #cd ~/git & git clone https://github.com/andrewssobral/bgslibrary.git
