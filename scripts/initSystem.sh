1f6b6e51c3d4a3364ef6faf2edd9703d355e2d6d
王佳欣 <youdaoyzbx@163.com>
2016-07-15 07:22:46 -0700

    Update initSystem.sh

--- scripts/initSystem.sh
+++ scripts/initSystem.sh
@@ -1,33 +1,34 @@
 ######!bin/bash
 
 #add opencv-nonfree model
-sudo add-apt-repository --yes ppa:xqms/opencv-nonfree
-sudo add-apt-repository --yes ppa:webupd8team/atom  
+#sudo add-apt-repository --yes ppa:xqms/opencv-nonfree
+sudo add-apt-repository --yes ppa:webupd8team/atom
 sudo add-apt-repository --yes ppa:george-edison55/cmake-3.x
-sudo add-apt-repository ppa:mc3man/trusty-media
+#sudo add-apt-repository ppa:mc3man/trusty-media
 #python ide
-sudo apt-add-repository ppa:ninja-ide-developers/ninja-ide-stable
+#sudo apt-add-repository ppa:ninja-ide-developers/ninja-ide-stable
 sudo apt-get update
-#sudo apt-get install atom 
+sudo apt-get install atom
 #sudo apt-get install ninja-ide
 sudo apt-get install build-essential -y
 sudo apt-get install libboost-dev -y
 sudo apt-get install make cmake autoconf -y
 sudo apt-get install pkg-config -y
 sudo apt-get install git -y
-sudo apt-get install qt5-default qtcreator qt5-doc -y
-sudo apt-get install libopencv-dev -y
-sudo apt-get install libopencv-nonfree-dev -y
+#sudo apt-get install qt5-default qtcreator qt5-doc -y
+#sudo apt-get install libopencv-dev -y
+#sudo apt-get install libopencv-nonfree-dev -y
 #sudo apt-get install motion
-sudo apt-get install nautilus-open-terminal -y
+sudo apt-get install nautilus-open-terminal libgnome2-bin-y
 sudo apt-get install cmake-gui -y
 sudo apt-get install tree -y
-sudo apt-get install libfftw3-dev -y
-sudo apt-get install python-dev
+sudo apt-get install synaptic -y
+#sudo apt-get install libfftw3-dev -y
+#sudo apt-get install python-dev
 # add ffmpeg support for opencv to use VideoCapture.
-sudo apt-get install libavformat-dev
-sudo apt-get install x264 v4l-utils ffmpeg
-sudo apt-get install ffmpeg
+#sudo apt-get install libavformat-dev
+#sudo apt-get install x264 v4l-utils ffmpeg
+#sudo apt-get install ffmpeg
 
 mkdir ~/git
 #cd ~/git & git clone https://github.com/andrewssobral/bgslibrary.git
