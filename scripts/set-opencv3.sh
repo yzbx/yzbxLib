40b52056b11e6bf8bcff7f13313b44e3a17da767
yzbx <youdaoyzbx@163.com>
2017-03-20 11:29:43 +0800

    add opencv set

---
+++ scripts/set-opencv3.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+Root=/usr/local/opencv3
+export LD_LIBRARY_PATH=$Root/lib
+export PKG_CONFIG_PATH=$Root/lib/pkgconfig
+export INCLUDE_PATH=$Root/include
+export PYTHONPATH=${PYTHONEPATH}:$Root/lib/python2.7/dist-packages
