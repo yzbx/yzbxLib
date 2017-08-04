602876f0ba60bb67229e84b2cdbb568c442821b2
王佳欣 <youdaoyzbx@163.com>
2017-08-04 17:02:13 +0800

    Update centos6Init.sh

--- scripts/centos6Init.sh
+++ scripts/centos6Init.sh
@@ -1,10 +1,10 @@
 # install opencv for centos!
 yum -y update
 yum list available opencv\*
-yum groupinstall 'Development Tools'
-yum install cmake git pkgconfig
-yum install libpng-devel libjpeg-turbo-devel jasper-devel openexr-devel libtiff-devel libwebp-devel
-yum install libdc1394-devel libv4l-devel gstreamer-plugins-base-devel
-yum install gtk2-devel
-yum install python-devel
-pip install numpy
+yum -y groupinstall 'Development Tools'
+yum -y install cmake git pkgconfig
+yum -y install libpng-devel libjpeg-turbo-devel jasper-devel openexr-devel libtiff-devel libwebp-devel
+yum -y install libdc1394-devel libv4l-devel gstreamer-plugins-base-devel
+yum -y install gtk2-devel
+yum -y install python-devel
+pip -y install numpy
